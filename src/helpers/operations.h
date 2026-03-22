a98a1390e6beb3f93e5cd2c7dc1e25907c8a212e
Gabriel-V-Maia <gabriel_v_maia@estudante.sesisenai.org.br>
2026-03-22 14:06:05 -0300

    feat: add PANIC and PANIC_F macros for error handling

---
+++ src/helpers/operations.h
@@ -0,0 +1,18 @@
+#import <stdio.h>
+#import <stdlib.h>
+
+#ifndef OPERATIONS_H
+#define OPERATIONS_H
+
+#define PANIC(fmt, ...) \
+    do { \
+        fprintf(stderr, "erro: " fmt "\n", ##__VA_ARGS__); \
+        exit(1); \
+    } while(0)
+
+#define PANIC_IF(cond, fmt, ...) \
+    do { \
+        if (cond) PANIC(fmt, ##__VA_ARGS__); \
+    } while(0)
+
+#endif
